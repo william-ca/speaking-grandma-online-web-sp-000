9bd12ec880627348a5e2e6f71d72e5e7fd3137e7
Will Andre <wandre94@gmail.com>
2020-12-13 19:47:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == !phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"  
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
