909ed5878907820954750df45f6897379cea0231
Will Andre <wandre94@gmail.com>
2020-12-13 19:37:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!" && phrase.upcase
-    puts "I LOVE YOU TOO PUMPKIN!"
+  if !phrase.upcase
+    puts "HUH?! SPEAK UP SONNY!"
   elsif !phrase.upcase
     puts "HUH?! SPEAK UP SONNY!"
   else
